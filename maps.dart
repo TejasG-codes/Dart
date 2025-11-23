fa989668a0b1ee5d51207c5cf7eb9cac71db7af9
Tejas Ghag <tejas.ghag@somaiya.edu>
2025-11-23 17:23:29 +0530

    Implement basic map operations in Dart
    Demonstrate basic operations on maps in Dart, including adding, modifying, and removing key-value pairs.

---
+++ maps.dart
@@ -0,0 +1,42 @@
+void main() {
+  // Maps are like dictionary in python
+  //Key: Value pair
+  var m1= { "a": "Apple",
+            "b":"Ball",
+            "c": "Cat"
+           };
+  print(m1);
+  
+  //Finding Value form KEY
+  print(m1["a"]);
+  
+  //Print only Values
+  print("Values: $m1.values");
+  
+  //Print only KEYS
+  print("Keys: $m1.keys");
+  
+  //Adding Things in Map
+  m1["d"]="Dog";
+  print(m1);
+  
+  //Adding MANY Things in Map
+  m1.addAll({"e":"Egg","f":"Fish","g":"Grape"});
+  print(m1);
+  
+  //Changing "Values"
+  m1["b"]="Bob";
+  print(m1);
+  print(m1["b"]);
+  
+  //Remove a KEY
+  m1.remove("f");
+  print(m1);
+  
+  //Remove all KEYS
+  m1.clear();
+  print(m1);
+  
+  
+  
+}
